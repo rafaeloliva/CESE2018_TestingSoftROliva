151905c5a5abcf6af5beea5882a386248328db37
rafaeloliva <roliva@uarg.unpa.edu.ar>
2018-10-14 22:28:03 -0300

    Todavia problemas con funciones sin, cos, atan2 (lm en linker) -desde ceedling?

---
+++ build/test/dependencies/cmock.d
@@ -0,0 +1,6 @@
+build/test/out/c/cmock.o: \
+ /home/rafael/CESE_DOCs/TestingSoftware/sp_stats/vendor/ceedling/vendor/cmock/src/cmock.c \
+ /home/rafael/CESE_DOCs/TestingSoftware/sp_stats/vendor/ceedling/vendor/unity/src/unity.h \
+ /home/rafael/CESE_DOCs/TestingSoftware/sp_stats/vendor/ceedling/vendor/unity/src/unity_internals.h \
+ /home/rafael/CESE_DOCs/TestingSoftware/sp_stats/vendor/ceedling/vendor/cmock/src/cmock.h \
+ /home/rafael/CESE_DOCs/TestingSoftware/sp_stats/vendor/ceedling/vendor/cmock/src/cmock_internals.h
---
+++ build/test/dependencies/stats_CL2_5sp.d
@@ -0,0 +1,1 @@
+build/test/out/c/stats_CL2_5sp.o: src/stats_CL2_5sp.c src/stats_CL2_5sp.h
